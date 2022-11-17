3d67640d7471b38c7db0df6ef71f689a42122637
alan88wy <alan88w@yahoo.com>
2022-11-17 15:03:00 +0800

    Change netlify address

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? "http://localhost:3000" : "https://salarymgmt-next.netlify.app"
+// export const server = dev ? "http://localhost:3000" : "https://salarymgmt-next.netlify.app"
+export const server ="https://salarymgmt-next.netlify.app"
 export const jwtSecret = 'ThisIsVerySecretive2022'
 
 
