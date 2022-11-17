5a719da331f9c8a39144eb98814fa67e450cbd3a
alan88wy <alan88w@yahoo.com>
2022-11-17 12:12:36 +0800

    Added netlify

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? "http://localhost:3000" : "https://mywebsite.com"
+export const server = dev ? "http://localhost:3000" : "https://salarymgmtNext.netlify.app"
 export const jwtSecret = 'ThisIsVerySecretive2022'
 
 // const j = require('crypto').randomBytes(64).toString('hex') // jwt secret
