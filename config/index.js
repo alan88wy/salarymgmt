52bd75f866932a22b6f7f5a75a57d8d63dfb2944
alan88wy <alan88w@yahoo.com>
2022-11-19 19:44:33 +0800

    Changes config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 const dev = process.env.NODE_ENV !== 'production'
+export const svr = process.env.server
 
-// export const server = dev ? "http://localhost:3000" : "https://salarymgmt-next.netlify.app"
-export const server = process.env.server
+export const server = dev ? "http://localhost:3000" : svr
+
 export const jwtSecret = 'ThisIsVerySecretive2022'
 
 
